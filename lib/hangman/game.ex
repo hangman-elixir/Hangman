1e9d0d7b5aa526e978ebe10b2797d53ca1105577
ihaveint <ihaveint@gmail.com>
2021-05-11 21:24:21 +0430

    Fix typo

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -2,10 +2,10 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    lettrs: []
+    letters: []
   )
 
   def new_game() do
-    %Hangman.Game{lettrs: Dictionary.random_word() |> String.codepoints()}
+    %Hangman.Game{letters: Dictionary.random_word() |> String.codepoints()}
   end
 end
