ae028f9a678f82604b6fe4dbd86e07bf959b454f
ihaveint <ihaveint@gmail.com>
2021-05-11 22:04:09 +0430

    Make sure the game state doesn't change after winning, or loosing

--- test/game_test.exs
+++ test/game_test.exs
@@ -10,4 +10,11 @@ defmodule GameTest do
     assert(length(game.letters) > 0)
     assert(Enum.all?(game.letters, fn x -> x >= "a" && x <= "z" && String.length(x) == 1 end))
   end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert {^game, _} = Game.make_move(game, "x")
+    end
+  end
 end
