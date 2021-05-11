7df76b56a559da3a3ce77222796ffd89e0dc93bb
ihaveint <ihaveint@gmail.com>
2021-05-12 00:57:33 +0430

    Add the logic for guessing a letter

--- test/game_test.exs
+++ test/game_test.exs
@@ -17,4 +17,35 @@ defmodule GameTest do
       assert {^game, _} = Game.make_move(game, "x")
     end
   end
+
+  test "first occurrences of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrences of letter is already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognized" do
+    game = Game.new_game("foo")
+    {game, _tally} = Game.make_move(game, "f")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is :won game" do
+    game = Game.new_game("foo")
+    {game, _tally} = Game.make_move(game, "f")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _tally} = Game.make_move(game, "o")
+    assert game.game_state == :won
+    assert game.turns_left == 7
+  end
 end
